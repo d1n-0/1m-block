341a0491032be14bb2c2b92419d43dbce72bb7db
Seohyun Jang <shjanghd@gmail.com>
2024-05-15 22:30:59 +0900

    Fix indent

--- iphdr.h
+++ iphdr.h
@@ -1,15 +1,15 @@
 #pragma once
 
 typedef struct {
-	#  if __BYTE_ORDER == __LITTLE_ENDIAN
+    #  if __BYTE_ORDER == __LITTLE_ENDIAN
     uint8_t ihl:4;
     uint8_t version:4;
-	#  endif
+    #  endif
 
-	#  if __BYTE_ORDER == __BIG_ENDIAN
+    #  if __BYTE_ORDER == __BIG_ENDIAN
     uint8_t version:4;
     uint8_t ihl:4;
-	#  endif
+    #  endif
     //uint8_t version_and_ihl;
 
     uint8_t type_of_service;
